f8ff61ec68095fbf149d7165e8b38246bdc533a2
BrenoMafra13 <brenolopesmafra@gmail.com>
2025-11-08 13:03:27 -0500

    Code from lab class week 10

--- microservices-parent/order-service/src/test/java/ca/gbc/comp3095/orderservice/OrderServiceApplicationTests.java
+++ microservices-parent/order-service/src/test/java/ca/gbc/comp3095/orderservice/OrderServiceApplicationTests.java
@@ -1,39 +1,46 @@
 package ca.gbc.comp3095.orderservice;
 
+import ca.gbc.comp3095.orderservice.stubs.InventoryClientStub;
+import com.github.tomakehurst.wiremock.WireMockServer;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.testcontainers.containers.PostgreSQLContainer;
-
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
-        @ServiceConnection
-        static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine");
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine");
 
-        @LocalServerPort
-        private Integer port;
+    @LocalServerPort
+    private Integer port;
 
-        @BeforeEach
-        void setup() {
-            RestAssured.baseURI = "http://localhost";
-            RestAssured.port = port;
-        }
+    @Autowired
+    private WireMockServer wireMockServer;
 
-        static {
-            postgreSQLContainer.start();
-        }
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
 
-        @Test
-        void placeOrderTest() {
-            String orderJson = """
+    static {
+        postgreSQLContainer.start();
+    }
+
+    @Test
+    void placeOrderTest() {
+        String orderJson = """
                 {
                   "skuCode": "samsung_tv_2025",
                   "price": 5000,
@@ -41,17 +48,20 @@ class OrderServiceApplicationTests {
                 }
                 """;
 
-            var responseBodyString = RestAssured
-                    .given()
-                    .contentType("application/json")
-                    .body(orderJson)
-                    .when()
-                    .post("/api/order")
-                    .then()
-                    .log().all()
-                    .statusCode(201)
-                    .extract()
-                    .body().asString();
-            assertThat(responseBodyString, Matchers.is("Order placed Successfully"));
-        }
+        InventoryClientStub.stubInventoryCall("samsung_tv_2025", 10);
+
+        var responseBodyString = RestAssured
+                .given()
+                .contentType("application/json")
+                .body(orderJson)
+                .when()
+                .post("/api/order")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .body().asString();
+
+        assertThat(responseBodyString, Matchers.is("Successfully Placed Order"));
     }
+}
--- microservices-parent/product-service/build.gradle.kts
+++ microservices-parent/product-service/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.5.6"
-	id("io.spring.dependency-management") version "1.1.7"
+    java
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "ca.gbc.comp3095"
@@ -9,36 +9,40 @@ version = "0.0.1-SNAPSHOT"
 description = "product-service"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.boot:spring-boot-testcontainers")
-	testImplementation("io.rest-assured:rest-assured")
-	testImplementation("org.testcontainers:junit-jupiter")
-	testImplementation("org.testcontainers:mongodb")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:mongodb")
+    testImplementation("io.rest-assured:rest-assured")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    testImplementation("org.testcontainers:redis")
+
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
