2f017460ba48b5e66c2ca317409abe2638515f0d
BrenoMafra13 <brenolopesmafra@gmail.com>
2025-11-10 14:18:02 -0500

    Code from lecture class week 11

--- microservices-parent/product-service/src/main/java/ca/gbc/comp3095/productservice/service/ProductServiceImpl.java
+++ microservices-parent/product-service/src/main/java/ca/gbc/comp3095/productservice/service/ProductServiceImpl.java
@@ -6,6 +6,9 @@ import ca.gbc.comp3095.productservice.model.Product;
 import ca.gbc.comp3095.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -13,65 +16,96 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/*
+ * 1. @Cacheable -> "Read from cache of exists, otherwise run method & cache result"
+ *    - value = "PRODUCT_CACHE"
+ *    - key = ""
+ *
+ * 2. @CachePut -> "Always run the method, then PUT the result in cache"
+ *    - Used to create/update so cache stays fresh
+ *    - key = "#result.id()"
+ *
+ * 3. @CacheEvict -> "Remove entry from cache"
+ *    - key: "#productId"
+ *
+ */
+
 @Service
-@RequiredArgsConstructor
 @Slf4j
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository _productRepository;
-    private final MongoTemplate _mongoTemplate;
+    private final MongoTemplate mongoTemplate;
 
     @Override
+    @CachePut(value = "PRODUCT_CACHE", key="#result.id()")
     public ProductResponse createProduct(ProductRequest productRequest) {
-        log.debug("Creating new product {}", productRequest);
+
+        log.debug("Create new product {}", productRequest);
 
         Product product = Product.builder()
                 .name(productRequest.name())
-                .price(productRequest.price())
                 .description(productRequest.description())
+                .price(productRequest.price())
                 .build();
 
+        //save product to database
         _productRepository.save(product);
-        log.debug("Saved product {}", product);
+        log.debug("Successfully saved new product {}", product);
+
+        /**
+         *
+         * Cache productCache = cacheManager.getCache("PRODUCT_CACHE");
+         * productCache.put(product.getId(), Product)
+         *
+         */
 
         return new ProductResponse(product.getId(), product.getName(),
                 product.getDescription(), product.getPrice());
     }
 
     @Override
+    @Cacheable(value = "PRODUCT_CACHE", key = "'ALL_PRODUCTS'")
     public List<ProductResponse> getAllProducts() {
-        log.debug("Retrieving all products");
 
+        log.debug("Returning a list of Products");
         List<Product> products = _productRepository.findAll();
 
-        return products.stream()
+        return products
+                .stream()
                 .map(this::mapToProductResponse)
                 .toList();
     }
 
-    private  ProductResponse mapToProductResponse(Product product) {
+    private ProductResponse mapToProductResponse(Product product) {
         return new ProductResponse(product.getId(), product.getName(),
                 product.getDescription(), product.getPrice());
     }
 
     @Override
+    @CachePut(value = "PRODUCT_CACHE", key="#result")
     public String updateProduct(String productId, ProductRequest productRequest) {
-        log.debug("Updating product with Id {}", productId);
-        Query query = Query.query(Criteria.where("id").is(productId));
-        Product updateProduct = _mongoTemplate.findOne(query, Product.class);
 
-        if(updateProduct != null){
-            updateProduct.setName(productRequest.name());
-            updateProduct.setDescription(productRequest.description());
-            updateProduct.setPrice(productRequest.price());
-            _productRepository.save(updateProduct).getId();
+        log.debug("Updating product with id {}",  productId);
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("id").is(productId));
+        Product product = mongoTemplate.findOne(query, Product.class);
+
+        if(product != null){
+            product.setName(productRequest.name());
+            product.setDescription(productRequest.description());
+            product.setPrice(productRequest.price());
+            return  _productRepository.save(product).getId();
         }
         return productId;
     }
 
     @Override
+    @CacheEvict(value = "PRODUCT_CACHE", key="#productId")
     public void deleteProduct(String productId) {
-        log.debug("Deleting product with id {}", productId);
+        log.debug("Deleting product with id {}",  productId);
         _productRepository.deleteById(productId);
     }
 }
